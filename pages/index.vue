339f29c41cf10b0905ff4bd1754946b9f3dac338
arimura <19s001shun@gmail.com>
2020-08-18 12:10:16 +0900

    blog

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://samplearimura.microcms.io/api/v1/blog',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': 'eb3bd0a2-28ab-44b5-bcb4-9a0c59edef96' }
+      }
+    )
+    return data
+  }
+}
+</script>
